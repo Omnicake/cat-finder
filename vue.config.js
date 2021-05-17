1b8990aaa0480f982a293f6ccf6895e615851ca5
Omnicake <gurub@mail.ru>
2021-05-17 13:31:36 +0600

    Another settings for deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    publicPath: '/cat-finder/',
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/cat-finder/'
+        : '/',
     transpileDependencies: [
         'vuetify'
     ],
